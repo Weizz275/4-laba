6c8f093c49cc74fab5214d4c2ccdac91a8f5603e
renatakhabirva <renatahabirovka49@gmail.com>
2024-10-16 18:01:07 +0500

    Lab6

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -19,10 +19,53 @@ namespace Shigapov_Autoservice_new
     /// Логика взаимодействия для AddEditPage.xaml
     /// </summary>
     public partial class AddEditPage : Page
+
     {
-        public AddEditPage()
+        private Service _currentServise = new Service();
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+            if (SelectedService != null)
+                _currentServise = SelectedService;
+
+            DataContext = _currentServise;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentServise.Title))
+                errors.AppendLine("Укажите название услуги");
+            if(_currentServise.Cost <= 0)
+            {
+                errors.AppendLine("Укажите правильно стоимость услуги");
+            }
+            if (_currentServise.Cost < 0 || _currentServise.Discount == null)
+            {
+                errors.AppendLine("Укажите правильно скидку");
+            }
+            if (string.IsNullOrWhiteSpace(_currentServise.Duration))
+            {
+                errors.AppendLine("Укажите длительность услуг");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentServise.ID == 0)
+                masterEntities.GetContext().Service.Add(_currentServise);
+            try
+            {
+                masterEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
+
--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -31,7 +31,7 @@ namespace Shigapov_Autoservice_new
 
         private void Button_click(object sender, RoutedEventArgs e)
         {
-                Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
@@ -96,6 +96,24 @@ namespace Shigapov_Autoservice_new
             if (RButtonUp.IsChecked.Value)
             {
                 ServiceListView.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                masterEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = masterEntities.GetContext().Service.ToList();
             }
         }
     }
